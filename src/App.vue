9d40432bff2a4e003845eea0b1117a9b0f46b36b
jocorrei <mgjoao41@gmail.com>
2022-10-29 12:37:41 +0100

    init tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeView msg="Hello Vue 3 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeView
   }
 }
 </script>
