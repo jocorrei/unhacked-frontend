ddec935b27ff78b952e9e878bacae7cf40e97ac9
jocorrei <mgjoao41@gmail.com>
2022-10-29 14:10:13 +0100

    vuetify font and start of home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
+  <div class="background" style="height: 120%; min-height: 100vh">
     <v-row>
-      <v-col>
-        <v-img src="@/assets/unhacked_logo.png">
-
-        </v-img>
+      <v-col align="center">
+        <v-btn variant="text" class="navButtons"> Submit proposal</v-btn>
       </v-col>
+      <v-col align="center">
+        <v-btn variant="text" class="navButtons"> Accept settlement</v-btn>
+      </v-col>
+      <v-col align="center">
+        <v-btn variant="text" class="navButtons"> Mission & Vision </v-btn>
+      </v-col >
+      <v-col align="center"> <v-btn>Connect Wallet</v-btn> </v-col>
     </v-row>
   </div>
 </template>
@@ -21,4 +26,20 @@
 </script>
 
 <style scoped>
+  .navButtons {
+    color: #ffffff;
+    font-family: "Kosugi";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+
+    line-height: 29px;
+  }
+  .background {
+    background: linear-gradient(
+      111.21deg,
+      #000000 58.96%,
+      rgba(0, 0, 0, 0.53) 108.36%
+    );
+  }
 </style>
