81d6f23e151c8bac768e3e05767ced5cb2b42fbc
jocorrei <mgjoao41@gmail.com>
2022-10-29 13:16:52 +0100

    checkpoint before vuetify

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <li class="py-4 flex">
-
       <div class="ml-3">
         <p class="text-sm font-medium text-gray-900">Name</p>
         <p class="text-sm text-gray-500">City</p>
